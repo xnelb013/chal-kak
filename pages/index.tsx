083e70e785500fad04077fc83c0ae3a10936df9a
Eunseok Choi <xnelb013@naver.com>
2023-08-13 21:55:16 +0900

    label : add gitmessage
    [description]
    깃 메시지 파일 생성
    
    [issue-number]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <div className="text-3xl">회원가입</div>
-      <div className="text-xl">SignUp</div>
+      <div className="text-3xl mt-20">회원가입</div>
+      <div className="text-xl mt-10">SignUp</div>
     </>
   );
 }

f45c75761280ab949c542a7b79ed3f722b586196
Eunseok Choi <xnelb013@naver.com>
2023-08-13 23:03:29 +0900

    [Setting] : eslintrc및 prettier 세팅 [description] eslintrc및 prettier 세팅 [issue-number]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <div className="text-3xl mt-20">회원가입</div>
-      <div className="text-xl mt-10">SignUp</div>
-    </>
-  );
+  return <div></div>;
 }

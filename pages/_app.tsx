6fba0ad0f9b50cc02fa0475b7f34772cdf7a2db5
Eunseok Choi <xnelb013@naver.com>
2023-08-13 20:50:08 +0900

    초기 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "./components/Navbar";
+import "../public/fonts/font.css";
+import "../public/fonts/notoSansKr.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="wrap">
+        <div className="container">
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ pages/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-3xl title">#찰칵</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+          </div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <Image src="/images/카카오.jpg" width={100} height={100} alt="profile-img" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

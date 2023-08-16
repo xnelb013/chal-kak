e48bbd48e69d0215a2c897fb62a60a98e1ff9531
khakisage <jongjin201@gmail.com>
2023-08-16 16:48:20 +0900

    [Feat] : msw handler 추가

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,10 +1,11 @@
 import { auth } from "@/firebase/firebaseAuth";
 import { db } from "@/firebase/firestore";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, deleteUser, signInWithEmailAndPassword } from "firebase/auth";
+import { deleteDoc, doc, getDoc, setDoc } from "firebase/firestore";
 import { rest } from "msw";
 
 export const handlers = [
+  // 회원가입 mocking API
   rest.post("http://localhost:3000/test", async (req, res, ctx) => {
     const { email, password, nickname, gender, height, weight } = await req.json();
     try {
@@ -20,4 +21,67 @@ export const handlers = [
       return res(ctx.status(400), ctx.json({ success: false }), ctx.json({ message: "회원 가입에 실패하였습니다." }));
     }
   }),
+  // 로그인 mocking API
+  rest.post("http://localhost:3000/signin", async (req, res, ctx) => {
+    const { email, password } = await req.json();
+    try {
+      await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        localStorage.setItem("user", JSON.stringify(userCredential.user));
+        alert("로그인에 성공하였습니다.");
+      });
+      return res(ctx.status(200), ctx.json({ success: true }), ctx.json({ message: "로그인에 성공하였습니다." }));
+    } catch (error) {
+      return res(ctx.status(400), ctx.json({ success: false }), ctx.json({ message: "로그인에 실패하였습니다." }));
+    }
+  }),
+  // 로그아웃 mocking API
+  rest.get("http://localhost:3000/signout", async (req, res, ctx) => {
+    try {
+      localStorage.removeItem("user");
+      return res(ctx.status(200), ctx.json({ success: true }), ctx.json({ message: "로그아웃에 성공하였습니다." }));
+    } catch (error) {
+      return res(ctx.status(400), ctx.json({ success: false }), ctx.json({ message: "로그아웃에 실패하였습니다." }));
+    }
+  }),
+  // 사용자 개인 정보 확인 mocking API
+  rest.get("http://localhost:3000/userinfo", async (req, res, ctx) => {
+    const user = localStorage.getItem("user");
+    const { email } = JSON.parse(user as string);
+    try {
+      getDoc(doc(db, "users", email)).then((doc) => {
+        console.log("userinfo", doc.data());
+      });
+      return res(
+        ctx.status(200),
+        ctx.json({ success: true }),
+        ctx.json({ message: "사용자 정보를 불러오는데 성공하였습니다." }),
+      );
+    } catch (error) {
+      return res(
+        ctx.status(400),
+        ctx.json({ success: false }),
+        ctx.json({ message: "사용자 정보를 불러오는데 실패하였습니다." }),
+      );
+    }
+  }),
+  // 회원 탈퇴 mocking API
+  // rest.post("http://localhost:3000/delete", async (req, res, ctx) => {
+  //   const user = localStorage.getItem("user");
+  //   const { email } = JSON.parse(user as string);
+  //   const curUser = auth.currentUser;
+  //   try {
+  //     // await getDoc(doc(db, "users", email)).then((doc) => {
+  //     //   if (doc.exists()) {
+  //     //     localStorage.removeItem("user");
+  //     //     doc.ref.delete();
+  //     //   } else {
+  //     //     console.log("No such document!");
+  //     //   }
+  //     await deleteUser(curUser!);
+  //     await deleteDoc(doc(db, "users", email));
+  //     return res(ctx.status(200), ctx.json({ success: true }), ctx.json({ message: "회원 탈퇴에 성공하였습니다." }));
+  //   } catch (error) {
+  //     return res(ctx.status(400), ctx.json({ success: false }), ctx.json({ message: "회원 탈퇴에 실패하였습니다." }));
+  //   }
+  // }),
 ];
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,3 +1,5 @@
+import { auth } from "@/firebase/firebaseAuth";
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Test() {
@@ -35,13 +37,9 @@ export default function Test() {
         alert("모든 항목을 입력해주세요.");
         return;
       }
-      const res = await fetch("/test", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password, nickname, gender, height, weight, agreement }),
-      });
-      if (res.ok) {
-        const data = await res.json();
+      const response = await axios.post("/test", { email, password, nickname, gender, height, weight, agreement });
+      if (response.status === 201) {
+        const data = response.data;
         console.log(data);
       }
     } catch (error) {
@@ -49,48 +47,101 @@ export default function Test() {
     }
   };
 
+  const handleSignin = async () => {
+    try {
+      if (!email || !password) {
+        alert("이메일과 비밀번호를 입력해주세요.");
+        return;
+      }
+      const res = await axios.post("/signin", { email, password });
+      if (res.status === 200) {
+        const data = res.data;
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUserInfo = async () => {
+    try {
+      const res = await axios.get("/userinfo");
+      if (res.status === 200) {
+        const data = res.data;
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      const res = await axios.post("/delete", { email });
+      if (res.status === 200) {
+        const data = res.data;
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log("1", auth.currentUser);
   return (
-    <div>
-      <div className="flex flex-col">
-        <label htmlFor="email">이메일</label>
-        <input type="text" id="email" name="email" value={email} onChange={handleOnChange} />
-        <label htmlFor="password">비밀번호</label>
-        <input type="password" id="password" name="password" value={password} onChange={handleOnChange} />
-        <label htmlFor="nickname">닉네임</label>
-        <input type="text" id="nickname" name="nickname" value={nickname} onChange={handleOnChange} />
-        <p>성별</p>
-        <label htmlFor="male">
-          <input
-            type="radio"
-            id="male"
-            name="gender"
-            value="male"
-            checked={gender === "male"}
-            onChange={handleOnChange}
-          />
-          남성
-        </label>
-        <label htmlFor="female">
-          <input
-            type="radio"
-            id="female"
-            name="gender"
-            value="female"
-            checked={gender === "female"}
-            onChange={handleOnChange}
-          />
-          여성
-        </label>
-        <label htmlFor="height">키</label>
-        <input type="number" id="height" name="height" value={height} onChange={handleOnChange} />
-        <label htmlFor="weight">몸무게</label>
-        <input type="number" id="weight" name="weight" value={weight} onChange={handleOnChange} />
-        <label htmlFor="agreement">
-          <input type="checkbox" id="agreement" name="agreement" checked={agreement} onChange={handleOnChange} />
-          이용약관에 동의합니다
-        </label>
-        <button onClick={handleSignup}>회원가입</button>
+    <>
+      <div>
+        <div className="flex flex-col">
+          <label htmlFor="email">이메일</label>
+          <input type="text" id="email" name="email" value={email} onChange={handleOnChange} />
+          <label htmlFor="password">비밀번호</label>
+          <input type="password" id="password" name="password" value={password} onChange={handleOnChange} />
+          <label htmlFor="nickname">닉네임</label>
+          <input type="text" id="nickname" name="nickname" value={nickname} onChange={handleOnChange} />
+          <p>성별</p>
+          <label htmlFor="male">
+            <input
+              type="radio"
+              id="male"
+              name="gender"
+              value="male"
+              checked={gender === "male"}
+              onChange={handleOnChange}
+            />
+            남성
+          </label>
+          <label htmlFor="female">
+            <input
+              type="radio"
+              id="female"
+              name="gender"
+              value="female"
+              checked={gender === "female"}
+              onChange={handleOnChange}
+            />
+            여성
+          </label>
+          <label htmlFor="height">키</label>
+          <input type="number" id="height" name="height" value={height} onChange={handleOnChange} />
+          <label htmlFor="weight">몸무게</label>
+          <input type="number" id="weight" name="weight" value={weight} onChange={handleOnChange} />
+          <label htmlFor="agreement">
+            <input type="checkbox" id="agreement" name="agreement" checked={agreement} onChange={handleOnChange} />
+            이용약관에 동의합니다
+          </label>
+          <button onClick={handleSignup}>회원가입</button>
+        </div>
       </div>
-    </div>
+      <div>
+        <div className="flex flex-col">
+          <label htmlFor="email">이메일</label>
+          <input type="text" id="email" name="email" value={email} onChange={handleOnChange} />
+          <label htmlFor="password">비밀번호</label>
+          <input type="password" id="password" name="password" value={password} onChange={handleOnChange} />
+          <button onClick={handleSignin}>로그인</button>
+        </div>
+      </div>
+      <button onClick={handleUserInfo}>유저정보</button>
+      <button onClick={handleDeleteUser}>유저삭제</button>
+    </>
   );
 }

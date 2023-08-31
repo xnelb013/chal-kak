0b42275da63a537ac0e9d00d7d7fc93fccab4dd3
xnelb013 <xnelb013@naver.com>
2023-08-31 13:20:09 +0000

    🔧 [Chore] : api url 변경

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: "https://ec2-13-127-154-248.ap-south-1.compute.amazonaws.com:443/",
+  baseURL: "http://ec2-13-127-154-248.ap-south-1.compute.amazonaws.com:8080/",
 });

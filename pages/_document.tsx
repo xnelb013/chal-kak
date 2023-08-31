bc7f4d17a0f1a0021abe685e2489fb05c7a1e267
xnelb013 <xnelb013@naver.com>
2023-08-31 13:40:58 +0000

    🚑 [Fix] : http 요청 에러

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <script src="https://kit.fontawesome.com/dfe08ffa54.js" crossOrigin="anonymous"></script>
       </Head>
       <body>

71d8afe07ad897aade925cac8445d613dedf6746
xnelb013 <xnelb013@naver.com>
2023-09-13 15:38:02 +0000

    🔧 [Chore] : 필요없는 부분 제거

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <script src="https://kit.fontawesome.com/dfe08ffa54.js" crossOrigin="anonymous"></script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />

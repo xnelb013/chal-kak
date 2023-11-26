587d31a1d1cecdeac1c612de022c607491981283
xnelb013 <xnelb013@naver.com>
2023-11-26 11:56:55 +0000

    🚑 [Fix] : 게시글 버튼 버그 - 게시글이 하나도 없는 경우 게시글 버튼이 보여지지 않음.

--- pages/main/GoToPostEditorButton.tsx
+++ pages/main/GoToPostEditorButton.tsx
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { AiOutlinePlus } from "react-icons/ai";
 
 const PostEditorButton = () => {
   const router = useRouter();
-  const [isVisble, setIsVisible] = useState(false);
+  const [isVisble] = useState(true);
 
   const handleNavigation = () => {
     router.push("/postEditor");
   };
-
+  // 게시글이 없는 경우, 게시글 작성 버튼이 보이지 않는 문제
   // 스크롤 이벤트 핸들러
-  const toggleVisibility = () => {
-    // if(window.scrollY > 300) { // 원하는 스크롤 위치에 도달했을 때 버튼이 보이도록 설정
-    //     setIsVisible(true);
-    // } else {
-    //     setIsVisible(false);
-    // }
-    setIsVisible(true);
-  };
+  // const toggleVisibility = () => {
+  //   if(window.scrollY > 300) { // 원하는 스크롤 위치에 도달했을 때 버튼이 보이도록 설정
+  //       setIsVisible(true);
+  //   } else {
+  //       setIsVisible(false);
+  //   }
+  //   setIsVisible(true);
+  // };
 
-  useEffect(() => {
-    window.addEventListener("scroll", toggleVisibility);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", toggleVisibility);
 
-    return () => window.removeEventListener("scroll", toggleVisibility);
-  }, []);
+  //   return () => window.removeEventListener("scroll", toggleVisibility);
+  // }, []);
 
   return (
     isVisble && (
       <button
         onClick={handleNavigation}
-        className="fixed top-[92%] right-[30px] border rounded-full border-gray-800 mb-2 text-white bg-gray-800 w-[42px] h-[42px] flex items-center justify-center"
+        className="fixed bottom-[5%] right-[30%] border rounded-full border-gray-800 mb-2 text-white bg-gray-800 w-[42px] h-[42px] flex items-center justify-center"
       >
         <AiOutlinePlus className="w-[24px] h-[24px]" />
       </button>
